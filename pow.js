b70f4d5aa47015a902f2efa8d6e87f68c6f5a5ec
Guruprasad Kamath <guru241987@gmail.com>
2021-09-10 20:00:58 +0200

    Add POW Solution

---
+++ pow.js
@@ -0,0 +1,109 @@
+#!node
+
+var crypto = require("crypto");
+
+// The Power of a Smile
+// by Tupac Shakur
+var poem = [
+	"The power of a gun can kill",
+	"and the power of fire can burn",
+	"the power of wind can chill",
+	"and the power of a mind can learn",
+	"the power of anger can rage",
+	"inside until it tears u apart",
+	"but the power of a smile",
+	"especially yours can heal a frozen heart",
+];
+
+var difficulty = 10;
+
+var Blockchain = {
+	blocks: [],
+};
+
+// Genesis block
+Blockchain.blocks.push({
+	index: 0,
+	hash: "000000",
+	data: "",
+	timestamp: Date.now(),
+});
+
+for (let line of poem) {
+	let bl = createBlock(line);
+	Blockchain.blocks.push(bl);
+	console.log(`Hash (Difficulty: ${difficulty}): ${bl.hash}`);
+
+	difficulty++;
+}
+
+
+// **********************************
+
+function createBlock(data) {
+	var bl = {
+		index: Blockchain.blocks.length,
+		prevHash: Blockchain.blocks[Blockchain.blocks.length-1].hash,
+		data,
+		timestamp: Date.now(),
+	};
+
+	bl.hash = blockHash(bl);
+
+	return bl;
+}
+
+function blockHash(bl) {
+	while (true){
+		bl.nonce = Math.floor(Math.random() * 1E7);;
+		let hash = crypto.createHash('sha256').update(JSON.stringify(bl)).digest('hex');
+
+		if (hashIsLowEnough(hash)){
+			return hash;
+		}
+	}
+}
+
+function hashIsLowEnough(hash) {
+	const compareChars = Math.ceil(difficulty/4) + 1;
+    // const zeroSuffixes = Math.min(compareChars*4 - difficulty - 1, 0);
+    // const zeroPrefixes = difficulty;
+
+	const threshold = Number('0b'.concat('0'.repeat(difficulty)).concat('1').padEnd('0', compareChars*4));
+	// console.log(`Difficulty: ${difficulty}; threshold: ${threshold});`);
+    
+    const compareHash = Number(`0x${hash.substr(0,compareChars)}`);
+
+	return compareHash <= threshold;
+}
+
+function verifyBlock(bl) {
+	if (bl.data == null) return false;
+	if (bl.index === 0) {
+		if (bl.hash !== "000000") return false;
+	}
+	else {
+		if (!bl.prevHash) return false;
+		if (!(
+			typeof bl.index === "number" &&
+			Number.isInteger(bl.index) &&
+			bl.index > 0
+		)) {
+			return false;
+		}
+		if (bl.hash !== blockHash(bl)) return false;
+	}
+
+	return true;
+}
+
+function verifyChain(chain) {
+	var prevHash;
+	for (let bl of chain.blocks) {
+		if (prevHash && bl.prevHash !== prevHash) return false;
+		if (!verifyBlock(bl)) return false;
+		prevHash = bl.hash;
+	}
+
+	return true;
+}
